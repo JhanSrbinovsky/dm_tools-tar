d5e5c54a21852fe38416de95b0a5b535de665c92
Srbinovsky, Jhan (O&A, Aspendale) <Jhan.Srbinovsky@csiro.au>
2026-08-06 16:18:23 +1000

    init scripts

---
+++ tar-n-split.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -P p66
+#PBS -q normal
+#PBS -l ncpus=1,walltime=24:00:00,mem=8Gb,wd
+#PBS -l storage=scratch/p73+gdata/p73
+#PBS -j oe
+#PBS -N tar-n-split
+
+tardir=tars
+tarname=`basename $wrapdir`
+
+if [[ ! -f $tardir/$tarname.tar.part-* ]]; then
+  echo "creating tar files: $tardir/$tarname.tar.part-*"
+  tar -C `dirname $wrapdir` -cvf - $tarname \
+    | split -d -b 2GB - "$tardir/$tarname.tar.part-"
+  echo "tars created successfully"
+else
+  echo "$tardir/$tarname.tar.part-* already exists, exiting"
+  exit
+fi
+
+exit
+
+# TO UNTAR:
+# cat ${tarname}.tar.part-* | tar -xvf -
---
+++ tar-n-split_wrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This script assumes: 
+#     that the tar-n-split.sh script is in the same directory as this wrapper.
+#     we are archiving production runs from p73 (@ $p73archive below)
+
+### user input req^d ###
+p73exp=test_archive/
+### END user input   ###
+
+#production archive is here 
+p73archive=/g/data/p73/archive/CMIP7/ACCESS-ESM1-6/production/
+
+# experiment to archive
+exp=$p73archive/$p73exp
+
+#qsub the job
+qsub -v "wrapdir=$exp" tar-n-split.sh
+
+exit
+
