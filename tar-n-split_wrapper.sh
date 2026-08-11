991d3cf733c73df54c3007e21a363c1af819bb6e
Srbinovsky, Jhan (O&A, Aspendale) <Jhan.Srbinovsky@csiro.au>
2026-08-11 10:44:56 +1000

    ammend comment

--- tar-n-split_wrapper.sh
+++ tar-n-split_wrapper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This script assumes: 
 #     that the tar-n-split.sh script is in the same directory as this wrapper.
-#     we are archiving production runs from p73 (@ $p73archive below)
+#     we are archiving production runs (as per experiment) from p73 (@ $p73archive below)
 
 ### user input req^d ###
-p73exp=test_archive/
+p73exp=esm-flat10_zec
 ### END user input   ###
 
 #production archive is here 
