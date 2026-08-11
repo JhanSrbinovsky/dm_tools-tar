966f5ad7353af790ee4c69c0d2e1570ecd3ff224
Srbinovsky, Jhan (O&A, Aspendale) <Jhan.Srbinovsky@csiro.au>
2026-08-11 11:10:29 +1000

    edit for experiment

--- tar-n-split.sh
+++ tar-n-split.sh
@@ -4,7 +4,7 @@
 #PBS -l ncpus=1,walltime=24:00:00,mem=8Gb,wd
 #PBS -l storage=scratch/p73+gdata/p73
 #PBS -j oe
-#PBS -N tar-n-split
+#PBS -N tns-flat10-cdr
 
 tardir=tars
 tarname=`basename $wrapdir`
@@ -12,7 +12,7 @@ tarname=`basename $wrapdir`
 if [[ ! -f $tardir/$tarname.tar.part-* ]]; then
   echo "creating tar files: $tardir/$tarname.tar.part-*"
   tar -C `dirname $wrapdir` -cvf - $tarname \
-    | split -d -b 2GB - "$tardir/$tarname.tar.part-"
+    | split -d -b 200GB - "$tardir/$tarname.tar.part-"
   echo "tars created successfully"
 else
   echo "$tardir/$tarname.tar.part-* already exists, exiting"
