c4c0a829d44c43c4e8ec1ada741b1583474c01ac
Srbinovsky, Jhan (O&A, Aspendale) <Jhan.Srbinovsky@csiro.au>
2026-08-11 10:56:50 +1000

    tar-n-split.sh.jobname

--- tar-n-split.sh
+++ tar-n-split.sh
@@ -4,7 +4,7 @@
 #PBS -l ncpus=1,walltime=24:00:00,mem=8Gb,wd
 #PBS -l storage=scratch/p73+gdata/p73
 #PBS -j oe
-#PBS -N tar-n-split
+#PBS -N tns-flat10-zec
 
 tardir=tars
 tarname=`basename $wrapdir`
